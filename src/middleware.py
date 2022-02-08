6597f79a464d58e8dce8cbe76e88ec4234d8e3df
Dylan Feng <dfeng102938@gmail.com>
2022-02-07 18:15:08 -0800

    Modified JSON input format

--- src/middleware.py
+++ src/middleware.py
@@ -1,4 +1,56 @@
+import json
+import pandas as pd
+from helper import *
 
 
-def json_to_pandas(data):
-    return 0
+def no_transform(x):
+    return x
+
+
+def categorize(x):
+    options = {}
+    curr_num = 0
+    for i, entry in enumerate(x):
+        if entry not in options:
+            options[entry] = curr_num
+            curr_num += 1
+
+        x[i] = options[entry]
+
+    return x
+
+
+def json_to_pandas(data_format):
+    tables = {}
+    data_format = json.loads(data_format)
+
+    for source in (data_format['inputs'] + data_format['predictions']):
+        if source['location'] not in tables:
+            if source['format'] == 'tabular': tables[source['location']] = pd.read_csv(source['location'])
+            else: continue
+
+        transformer = transformers[source['transformer']]
+        table = tables[source['location']]
+        col_data = table[source['name']].tolist()
+        col_data = transformer(col_data)
+        table[source['name']] = col_data
+
+    for table_name in tables:
+        print(tables[table_name].head())
+
+
+transformers = {
+    "none": no_transform,
+    "categorize": categorize
+}
+
+
+def main():
+    with open("../test/input-2.json") as f:
+        data_format = f.read()
+
+    json_to_pandas(data_format)
+
+
+if __name__ == "__main__":
+    main()
